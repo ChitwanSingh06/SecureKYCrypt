b877d0bab48c6d73c5e7bf178087145840018456
Shruthi K <kshruthi206@gmail.com>
2026-02-25 18:55:56 +0530

    running code

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,11 @@
 from flask import Flask
+from flask_cors import CORS
 from routes.verify_routes import verify_bp
 
 app = Flask(__name__)
+CORS(app)
+
 app.register_blueprint(verify_bp)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
